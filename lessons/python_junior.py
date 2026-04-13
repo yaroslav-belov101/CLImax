f446d67953b98f04be34114b08b73cc3a935c1c2
Yaroslav Belov <yarbelov17@gmail.com>
2026-04-13 21:38:21 +0300

    Version 0.1.1 Поправил урок по Python

--- lessons/python_junior.py
+++ lessons/python_junior.py
@@ -12,20 +12,20 @@ class PythonJuniorLesson(BaseLesson):
 
     @property
     def description(self) -> str:
-        return "Базовые команды Python в CLI"
+        return "Базовые команды Python"
 
     @property
     def steps(self) -> list[LessonStep]:
         return [
             LessonStep(
-                command="python --version",
-                description="Проверьте установленную версию Python",
-                hint="или python3 --version",
+                command="print",
+                description="Выводит на экран содержимое",
+                hint="print('Hellow world')",
             ),
             LessonStep(
-                command="python -c \"print('Hello World')\"",
-                description="Выполните Python-код прямо в командной строке",
-                hint="Флаг -c позволяет передать код строкой",
+                command="input()",
+                description="Создание переменных",
+                hint="a = input()",
                 strict=True,
             ),
             LessonStep(
